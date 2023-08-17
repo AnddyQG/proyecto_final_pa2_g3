6fc193322b79437b5e2606204830a43d0761b6c6
AnddyQG <anddysk819@gmail.com>
2023-08-16 22:26:30 -0500

    sin la funcionalidad 2.e

---
+++ src/main/java/com/example/demo/controller/ClienteController.java
@@ -0,0 +1,107 @@
+package com.example.demo.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.repository.modelo.Cliente;
+import com.example.demo.repository.modelo.Vehiculo;
+import com.example.demo.repository.modelo.dto.ReservaDto;
+import com.example.demo.service.IClienteService;
+import com.example.demo.service.IReservaService;
+import com.example.demo.service.IVehiculoService;
+
+@Controller
+@RequestMapping("/clientes")
+
+public class ClienteController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ClienteController.class);
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@Autowired
+	private IVehiculoService iVehiculoService;
+
+	@Autowired
+	private IReservaService iReservaService;
+
+	LocalDateTime fInicio = null;
+	LocalDateTime fFin = null;
+	String ced = null;
+	String tarj = null;
+
+	@GetMapping("/opciones")
+	public String vistaCliente() {
+
+		return "vistaCliente";
+	}
+
+	@GetMapping("/registrarCli")
+	public String vistaRegistrarCliente(Cliente cliente) {
+
+		return "vistaRegistrarCliente";
+
+	}
+
+	@PostMapping("/insertar")
+	public String insertarCliente(Cliente cliente) {
+
+		cliente.setRegistro("C");
+		this.clienteService.registrarC(cliente);
+
+		return "ClieGuardado";
+	}
+
+	@GetMapping("/vehiculosDisponibles")
+	public String vistaVehiculosDisponibles(Model model, Vehiculo vehiculo) {
+
+		List<Vehiculo> listaVehiculos = this.iVehiculoService.buscarTodosVehi();
+		model.addAttribute("vehiculos", listaVehiculos);
+		return "vistaListaDisponibles";
+
+	}
+
+	@GetMapping("/registrarReserva")
+	public String vistaInsertarReserva(Model model) {
+
+		model.addAttribute("reserva", new ReservaDto());
+
+		return "vistaRegistrarReserva";
+	}
+
+	
+	@PostMapping("/reservar")
+	public String insertarReserva(@ModelAttribute ReservaDto dto, RedirectAttributes attributes) {
+
+	String mensaje =this.iReservaService.reservarRetorno(dto.getPlaca(), dto.getCedula(), dto.getInicio(), dto.getFin(),
+			dto.getNumeroTarjeta());
+
+		
+		if (mensaje.equals("Reserva Exitosa")) {
+			//CREAR EN ESTE REDIRECT QUE NOS MANDE A UNA PAGINA NUEVA QUE DIGA RESERVA EXITOSA
+			return "redirect:/clientes/reservaExitosa"; // Redirige a la lista de reservas
+        } else {
+            attributes.addFlashAttribute("mensajeError", mensaje);
+            return "redirect:/clientes/registrarReserva";// Redirige de vuelta al formulario
+        }
+		
+	}
+	
+	@GetMapping("/reservaExitosa")
+	public String vistaReservaExitosa() {
+		
+		return "vistaReservaExitosa";
+	}
+}
---
+++ src/main/java/com/example/demo/controller/ReporteController.java
@@ -0,0 +1,107 @@
+package com.example.demo.controller;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.repository.modelo.Reserva;
+import com.example.demo.repository.modelo.Vehiculo;
+import com.example.demo.repository.modelo.dto.ReporteDto;
+import com.example.demo.repository.modelo.dto.ReporteVehiculoDto;
+import com.example.demo.repository.modelo.dto.VehiculoDto;
+import com.example.demo.service.IReservaService;
+import com.example.demo.service.IVehiculoService;
+
+@Controller
+@RequestMapping("/reportes")
+public class ReporteController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ClienteController.class);
+	
+	@Autowired
+	private IReservaService iReservaService;
+	@Autowired
+	private IVehiculoService  iVehiculoService;
+	
+	
+	@GetMapping("/opciones")
+	public String vistaReporte() {
+
+		return "vistaReporte";
+	}
+
+	
+	
+	@GetMapping("/buscar")
+	public String buscarR(ReporteDto dto,VehiculoDto dto2) {
+		
+		
+		return "vistaBuscarReporte";
+	}
+	
+	@GetMapping("/consultarRangoFechas")
+	public String buscarRangoFechas(ReporteDto dto,Model model) {
+		
+		List<Reserva> reservas=this.iReservaService.buscarRangoFecha(LocalDateTime.parse(dto.getInicio()),LocalDateTime.parse(dto.getFin()));
+		model.addAttribute("reservas",reservas);
+		
+		
+		
+		 return "vistaRangoFechasReserva";
+	}
+	
+	//buscar Clientes vip
+	@GetMapping("/buscarVip")
+	public String buscarCliVip(Model model) {
+		
+		List<Reserva>reservas=this.iReservaService.buscarClientesVip();
+		model.addAttribute("reservas",reservas);
+		
+		return "vistaBuscarCliVip";
+	}
+	//reporte vehiculos Vip
+	
+	
+	
+	@GetMapping("/reporteVehiculo")
+	public String reporteVehiculosVip(Reserva reserva) {
+		return "vistaReporteVehiVip";
+		
+	}
+	
+	@GetMapping("/buscarVipVehiculo")
+	public String buscarVehiculosVip(Reserva dto,Model model) {
+		
+		String fecha = "%" + dto.getEstado() + "%";
+		
+		List<Vehiculo>vehiculos= this.iVehiculoService.buscarVehiculoVip(fecha);
+		
+		LOG.info(fecha);
+		
+		List<ReporteVehiculoDto>dtos=this.iReservaService.ReporteEncontrarVehi(this.iVehiculoService.buscarVehiculoVip(fecha));
+		
+		String pag;
+		
+		if(dtos.isEmpty()) {
+			return "redirect:/reportes/reporteVehiculo";
+		}else {
+			model.addAttribute("reporte",dtos.stream().sorted(Comparator.comparing(ReporteVehiculoDto::getValoTotal).reversed()).collect(Collectors.toList()));
+			
+			
+		}
+		
+		
+		return "vistaVehiculosVip";
+	}
+
+	
+}
